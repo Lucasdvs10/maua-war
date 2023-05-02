ca56dcb9647d08ee4afb78d6e0ee6d71996ec2bd
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-05-02 09:13:29 -0300

    Entidade Block implementada e testada

---
+++ src/domain/entities/Block.java
@@ -0,0 +1,52 @@
+package domain.entities;
+
+public class Block {
+    private String _blockName;
+    private int _currentLife;
+    private int _maxLife;
+
+
+    public Block(String _blockName, int _maxLife) {
+        this._blockName = _blockName;
+        this.set_maxLife(_maxLife);
+        this.set_currentLife(_maxLife);
+    }
+    public Block(String _blockName, int _maxLife, int _currentLife) {
+        this._blockName = _blockName;
+        this.set_maxLife(_maxLife);
+        this.set_currentLife(_currentLife);
+    }
+
+    public void set_blockName(String _blockName) {
+        this._blockName = _blockName;
+    }
+
+    public void set_currentLife(int _currentLife) {
+        if(_currentLife <= 0)
+            this._currentLife = 0;
+        else if(_currentLife >= this._maxLife){
+            this._currentLife = this._maxLife;
+        }
+        else{
+            this._currentLife = _currentLife;
+        }
+    }
+
+    public void set_maxLife(int _maxLife) {
+        if( _maxLife <= 0) return;
+
+        this._maxLife = _maxLife;
+    }
+
+    public String get_blockName() {
+        return _blockName;
+    }
+
+    public int get_currentLife() {
+        return _currentLife;
+    }
+
+    public int get_maxLife() {
+        return _maxLife;
+    }
+}
---
+++ src/tests/unit/BlockEntityShould.java
@@ -0,0 +1,35 @@
+package tests.unit;
+
+import domain.entities.Block;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BlockEntityShould {
+    @Test
+    public void Set_Current_Life_As_5_When_Bigger_Than_MaxLife(){
+        Block myBlock = new Block("A", 5);
+
+        myBlock.set_currentLife(10);
+
+        Assertions.assertEquals(5, myBlock.get_currentLife());
+    }
+
+
+    @Test
+    public void Set_Current_Life_As_0_When_Less_Than_Zero(){
+        Block myBlock = new Block("A", 5);
+
+        myBlock.set_currentLife(-1);
+
+        Assertions.assertEquals(0, myBlock.get_currentLife());
+    }
+
+
+    @Test
+    public void Set_Max_Life_As_0_When_Less_Than_Zero(){
+        Block myBlock = new Block("A", -5);
+
+
+        Assertions.assertEquals(0, myBlock.get_maxLife());
+    }
+}
